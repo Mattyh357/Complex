5ef5d2bc284866358a4ed6a87fce961496c2b92d
Matt <mattyh357@gmail.com>
2023-11-05 22:14:41 +0000

    Update MQTTClient.py
    Adding keep_alive, qos, and retain settings

--- app/MQTTClient.py
+++ app/MQTTClient.py
@@ -7,13 +7,32 @@ class MQTTClient:
     port: int
     client: mqtt_client.Client
 
+    # TODO default
+    keep_alive: int
+    qos: int
+    retain: bool
+
+
     topics: []
 
-    def __init__(self, broker, port, topics):
+    def __init__(self, broker, port, topics, keepalive=60, qos=0, retain=False, tls=None, auth=None):
         self.broker = broker
         self.port = port
         self.client = mqtt_client.Client()
         self.topics = topics
+        self.keep_alive = keepalive
+        self.qos = qos
+        self.retain = retain
+
+        #TODO retain
+        #TODO tls
+        #TODO auth
+
+        # if tls:
+        #     self.client.tls_set(tls)
+
+        # if auth:
+        #     self.client.username_pw_set(username, password)
 
         # Assign event callbacks
         self.client.on_connect = self.on_connect
@@ -37,5 +56,9 @@ class MQTTClient:
         self.client.disconnect()
 
     # Publish a message to a topic
-    def publish(self, topic, message):
-        self.client.publish(topic, message)
+    def publish(self, topic, message, qos=None, retain=None):
+        qos = self.qos if qos is None else qos
+        retain = self.retain if retain is None else retain
+
+        # send
+        self.client.publish(topic, message, qos, retain)
