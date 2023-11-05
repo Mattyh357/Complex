3b5a3f257045938cfd03812863f1fcdf321ab9aa
Matt <mattyh357@gmail.com>
2023-11-05 14:55:09 +0000

    Update WebApp.py
    Basic data emit using socketio

--- app/WebApp.py
+++ app/WebApp.py
@@ -2,6 +2,7 @@ import sys
 import time
 from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO, emit
+import eventlet
 
 
 class WebApp:
@@ -12,6 +13,8 @@ class WebApp:
     port = 1234
 
     def __init__(self, ip, port):
+        eventlet.monkey_patch()
+
         self.flask = Flask(__name__, static_folder='web', template_folder='web')
         self.socketio = SocketIO(self.flask, cors_allowed_origins="*")
 
@@ -19,6 +22,9 @@ class WebApp:
         def index():
             return render_template('index.html')
 
+    def send_data(self, data):
+        print("Emitting:", data)
+        self.socketio.emit('data', data)
 
     def start(self):
         self.socketio.run(self.flask, host='0.0.0.0', port=self.port, debug=False)
