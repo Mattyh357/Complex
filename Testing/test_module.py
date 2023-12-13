bc5248c5133e8e61a7496c19a5a14ec1707d5e75
Matt <mattyh357@gmail.com>
2023-12-13 13:08:39 +0000

    Move test into test folder

--- Testing/test_module.py
+++ Testing/test_module.py
@@ -5,24 +5,29 @@ import time
 import sys
 import os
 
-sys.path.append(os.path.abspath('./app'))
-
+sys.path.append(os.path.abspath('./Complex'))
 import led_module
 
 
 # Initialize the LED connected to GPIO pin 20 (or whichever GPIO pin you are using)
-led_module.init_led("20")
+led_module.init_led("16","20","21")
 
 # Blink the LED 5 times with 1-second intervals
 
-led_module.startBlinking()
+led_module.red_on()
 time.sleep(1)
-led_module.turn_off()
+led_module.red_off()
 time.sleep(1)
-led_module.turn_on()
-time.sleep(1)
-led_module.turn_off()
 
+led_module.yellow_on()
+time.sleep(1)
+led_module.yellow_off()
+time.sleep(1)
+
+led_module.green_on()
+time.sleep(1)
+led_module.green_off()
+time.sleep(1)
 
 # for _ in range(5):
 #     led_module.turn_on()
